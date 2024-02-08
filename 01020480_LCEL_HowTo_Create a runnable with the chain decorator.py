345e1621546ca22a5f30c8723bf81bc336b92949
MartinChen1973 <cheny@cheny.com>
2024-02-08 18:17:55 +0800

    Create 01020480_LCEL_HowTo_Create a runnable with the chain decorator.py

---
+++ 01020480_LCEL_HowTo_Create a runnable with the chain decorator.py
@@ -0,0 +1,21 @@
+# Load the API key from the .env file   从.env文件中加载API密钥
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import chain
+from langchain_openai import ChatOpenAI
+
+prompt1 = ChatPromptTemplate.from_template("Tell me a joke about {topic}")
+prompt2 = ChatPromptTemplate.from_template("What is the subject of this joke: {joke}")
+
+@chain
+def custom_chain(text):
+    prompt_val1 = prompt1.invoke({"topic": text})
+    output1 = ChatOpenAI().invoke(prompt_val1)
+    parsed_output1 = StrOutputParser().invoke(output1)
+    chain2 = prompt2 | ChatOpenAI() | StrOutputParser()
+    return chain2.invoke({"joke": parsed_output1})
+
+print(custom_chain.invoke("bears"))
